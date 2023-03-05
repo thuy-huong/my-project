890168ceb57fc3c2a12f9413b860556a7570680e
thuy-huong <thuyhuong31804@gmail.com>
2023-03-05 19:09:28 +0700

    bai tap nhap xuat

---
+++ nhap_xuat.cpp
@@ -0,0 +1,13 @@
+//nhap-xuat
+#include <stdio.h>
+int main(){
+	int a=5;
+	float b=6;
+	printf("hien thi so a %d \n",a);
+	printf("hien thi so b %.1f \n",b);
+	printf("Hien thi ca a va b: %d và %.2f",a,b);
+	int c;
+	printf("\nNhap so c:");
+	scanf("%d",&c);
+	printf("Hien thi so c %d",c);
+}
